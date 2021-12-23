95a28770dd9c79e1bb9e530c0286f6f1878913a4
Gabriel Moisan Matte <gabrielmatte@msn.com>
2021-12-23 08:16:09 -0500

    Fixes Ports + Adds configuration template

--- launch.sh
+++ launch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ ./* == "./*" ]; then
+    mv /usr/src/tmp/* /usr/src/pavlovserver/Pavlov/Saved
+fi
+rm -r /usr/src/tmp
 
 # Check for pavlov updates
 echo "Updating Pavlov"
@@ -7,12 +11,13 @@ echo "Updating Pavlov"
 # check for sdk updates
 echo "Updating SDK64"
 /usr/games/steamcmd +force_install_dir /usr/src/sdk64 +login anonymous +app_update 1007 +quit
-cp /usr/src/sdk64/steamclient.so /home/steam/.steam/sdk64/steamclient.so
-cp /usr/src/sdk64/steamclient.so /usr/src/pavlovserver/Pavlov/Binaries/Linux/steamclient.so
+cp -rf /usr/src/SteamSDK/linux64/steamclient.so /home/steam/.steam/sdk64/steamclient.so
+cp -rf /usr/src/SteamSDK/linux64/steamclient.so /usr/src/pavlovserver/Pavlov/Binaries/Linux/steamclient.so
 
 # Set permissions + change to user
 echo "Changing folders permissions"
-chown steam:steam -R /usr/src/*
+chown steam:steam -R /usr/src
+chown steam:steam -R /home/steam
 
 echo "Log in steam user"
 runuser -l steam -c bash<<_
