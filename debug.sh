a5df3aee2293011732b4b13f24bd91983e183f44
Gabriel Moisan Matte <gabrielmatte@msn.com>
2021-12-23 21:19:33 -0500

    Adds debug scripts

---
+++ debug.sh
@@ -0,0 +1,1 @@
+docker run -d --name=pavlov -e TZ=America/New_York -e PUID=1000 -e PGID=1000 -p 7777:7777/tcp -p 7777:7777/udp -p 8177:8177/tcp -p 8177:8177/udp -p 9100:9100 -v /mnt/d/Pavlov/Config/:/usr/src/pavlovserver/Pavlov/Saved/ pavlovshackdocker:latest
